fbea146b81e2d4646c0dfce525d0c49ff52ce71a
Refaat Rady <refaattaha2020@gmail.com>
2026-04-29 01:45:22 +0100

    Update main.tf

--- modules/aws-ec2/main.tf
+++ modules/aws-ec2/main.tf
@@ -88,11 +88,11 @@ resource "aws_autoscaling_group" "nginx_asg" {
   target_group_arns         = [var.target_group_arn]
   health_check_type         = "ELB"
   health_check_grace_period = 120
-lifecycle {
+  lifecycle {
 
-    ignore_changes = [desired_capacity]
+      ignore_changes = [desired_capacity]
 
-  }
+    }
   tag {
     key                 = "Name"
     value               = "nginx-asg"
